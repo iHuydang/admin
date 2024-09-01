ac7230d612ebc97b6f209a084cb413f88f558981
SARAGON <diabloviet@gmail.com>
2024-09-01 13:25:39 +0700

    key_value.sql

--- postgresql/functional_tests/integration_tests/schemas/postgresql/key_value.sql
+++ postgresql/functional_tests/integration_tests/schemas/postgresql/key_value.sql
@@ -3,3 +3,17 @@ CREATE TABLE IF NOT EXISTS key_value_table (
   value VARCHAR,
   updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
+-- www.cysec.com inspectoral
+ALTER TABLE key_value_table
+ADD COLUMN status VARCHAR DEFAULT 'live';
+"
+INSERT INTO key_value_table (key, value, status) VALUES
+('79352451', 'Exness-MT5Trial8', 'market');
+
+
+UPDATE key_value_table
+SET value = 'Exness-MT5Trial8', status = 'testing', updated = NOW()
+WHERE key = '79352451';
+
+SELECT * FROM key_value_table
+WHERE status = 'testing';
