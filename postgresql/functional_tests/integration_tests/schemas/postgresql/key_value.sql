8f51a5096b07bf20583c7f974dfa662b53e44085
SARAGON <diabloviet@gmail.com>
2024-09-01 13:15:07 +0700

    Update key_value.sql

--- postgresql/functional_tests/integration_tests/schemas/postgresql/key_value.sql
+++ postgresql/functional_tests/integration_tests/schemas/postgresql/key_value.sql
@@ -3,3 +3,7 @@ CREATE TABLE IF NOT EXISTS key_value_table (
   value VARCHAR,
   updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
+SELECT * FROM key_value_table;
+UPDATE key_value_table
+SET value = 'new_value_for_key_1', updated = NOW()
+WHERE key = 'example_key_1';
